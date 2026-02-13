2f295a2a8792b914849d8398e26ff25b53353957
Malavika Gopinath <malavika.gopinath123@gmail.com>
2026-02-13 14:19:12 +0100

    feat(user): add UserController with full CRUD endpoints

--- backend/src/RoleNest.Application/Interfaces/Services/IUserService.cs
+++ backend/src/RoleNest.Application/Interfaces/Services/IUserService.cs
@@ -5,6 +5,7 @@ namespace RoleNest.Application.Interfaces.Services;
 public interface IUserService
 {
     Task<User?> GetByIdAsync(Guid id);
+    Task<IEnumerable<User>> GetAllAsync();
     Task<User> CreateAsync(User user);
     Task UpdateAsync(User user);
     Task DeleteAsync(Guid id);
